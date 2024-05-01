82092e87b5d14e30aeccd759ab6c87f73abfb1a5
wandersonss2 <wandersonssilva0@gmail.com>
2024-05-01 13:42:25 -0300

    Removidos arquivos que não eram para ser enviados

--- config/config.js
+++
@@ -1,5 +0,0 @@
-
-module.exports={
-    secretKey: 'dasdasdqweawsdaedqweeqeaweawsfdasede',
-    MONGO_URL: 'mongodb+srv://wanderson:93230167@cluster0.zapb3j7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-};
